18399ecc1cbe172c1175e5c9869d0581a00aa6a4
MarceloIlegra <marcelo.serpa@ilegra.com>
2016-09-11 14:14:02 -0300

    investigating error long request

--- kafka-samples/try-consumer/src/main/java/com/mserpa/kafkasample/producer/KafkaConsumerFun.java
+++ kafka-samples/try-consumer/src/main/java/com/mserpa/kafkasample/producer/KafkaConsumerFun.java
@@ -3,6 +3,8 @@ package com.mserpa.kafkasample.producer;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.common.TopicPartition;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -14,6 +16,8 @@ public class KafkaConsumerFun {
     private final static String URL_KAFKA = "localhost:9092,localhost:9093, localhost:9094";
     private final static String TOPIC_KAFKA = "my-replicated-topic";
 
+    private static Logger LOG = LogManager.getLogger(KafkaConsumerFun.class);
+
     private KafkaConsumer<String, String> consumer;
 
     public KafkaConsumerFun() {
@@ -22,66 +26,78 @@ public class KafkaConsumerFun {
         props.put("group.id", "test");
         props.put("enable.auto.commit", "false");
         props.put("auto.commit.interval.ms", "0");
-
-        props.put("request.timeout.ms", "60000");
-        props.put("session.timeout.ms", "50000");
+        props.put("request.timeout.ms", "200000");
+        props.put("session.timeout.ms", "190000");
         //props.put("consumer.timeout.ms","30000");
-
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 
         this.consumer = new KafkaConsumer<>(props);
 
         this.consumer.subscribe(Arrays.asList(TOPIC_KAFKA));
-        System.out.println("configurated");
+        LOG.info("configurated");
     }
 
     public void run(){
 
+        LOG.info("Starting main loop");
         while (true) {
             Long currentOffset = null;
             Boolean hasError = false;
-            System.out.println("Before poll");
+            LOG.info("Fetching poll messages...");
             ConsumerRecords<String, String> records = this.consumer.poll(100);
-            System.out.println("After poll: " + records.count());
+            LOG.info("Fetched " + records.count() + " messages.");
             for (ConsumerRecord<String, String> record : records) {
                 currentOffset = record.offset();
                 String message = record.value();
-                System.out.println("Message: " + message);
+                LOG.info(String.format("Processing message: %s offset: %s", message, currentOffset));
                 if(StringUtils.isNumeric(message)){
-                    System.out.println("Sleeping for " + message + " ms.");
+                    LOG.info("Sleeping for " + message + " ms.");
                     try {
                         Thread.sleep(Long.valueOf(message));
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
-                commitOffset(hasError, record);
+                boolean commitCompleted = commitOffset(hasError, record);
+                if(!commitCompleted) break;
             }
+            LOG.info("Has Error " + hasError);
             if (hasError) keepOnCurrentOffset(currentOffset);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+
         }
 
     }
 
     private void keepOnCurrentOffset(Long currentOffset) {
+        LOG.info("Keep on current offset");
         TopicPartition topicPartition = new TopicPartition(URL_KAFKA, 0);
         consumer.seek(topicPartition, currentOffset);
     }
 
-    private void commitOffset(Boolean hasError, ConsumerRecord<String, String> record) {
+    private Boolean commitOffset(Boolean hasError, ConsumerRecord<String, String> record) {
+        LOG.info("Preparing to commit");
         Map<TopicPartition, OffsetAndMetadata> map = new HashMap<>();
         TopicPartition topicPartition = new TopicPartition(TOPIC_KAFKA, record.partition());
         map.put(topicPartition, nextOffset(record));
-        consumer.commitSync(map);
+        LOG.info("Started commit");
+        try {
+            consumer.commitSync(map);
+            LOG.info("Ended commit");
+        }catch(CommitFailedException ex){
+            LOG.info("Commit cannot be completed ");
+            ex.printStackTrace();
+            return false;
+        }
+        return true;
     }
 
     private static OffsetAndMetadata nextOffset(ConsumerRecord<String, String> record) {
-        return new OffsetAndMetadata(record.offset() + 1);
+        LOG.info("Going to next offset");
+        LOG.info("Current offset " + record.offset());
+        OffsetAndMetadata offset = new OffsetAndMetadata(record.offset() + 1);
+        LOG.info("Next offset" + offset.offset());
+        return offset;
     }
 
 
--- kafka-samples/try-producer/src/main/java/com/mserpa/kafkasample/producer/KafkaProducerSample.java
+++ kafka-samples/try-producer/src/main/java/com/mserpa/kafkasample/producer/KafkaProducerSample.java
@@ -1,14 +1,20 @@
 package com.mserpa.kafkasample.producer;
 
-import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
+
 public class KafkaProducerSample {
+
+    private static Logger LOG = LogManager.getLogger(KafkaProducerSample.class);
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         Properties props = new Properties();
@@ -21,7 +27,26 @@ public class KafkaProducerSample {
 
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
 
-        producer.send(new ProducerRecord<>("my-replicated-topic", "Hello World - High Level Producer")).get();
+        List<String> messages = new ArrayList<String>();
+
+        messages.add("70000");
+        messages.add("80000");
+        messages.add("90000");
+        messages.add("100000");
+        messages.add("110000");
+
+        messages.stream()
+            .forEach(m -> {
+                try {
+                    LOG.info("Preparing to send message:" + m);
+                    producer.send(new ProducerRecord<>("my-replicated-topic", m)).get();
+                    LOG.info("Sent message:" + m);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+            });
 
         producer.flush();
         System.out.println("...End");
